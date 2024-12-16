84ec7f60c5a1e4ff13f70d527e7ae7b742510aa6
ReFi DAO <167816643+ReFiDAO@users.noreply.github.com>
2024-12-16 13:58:00 +0000

    First update fundraisers.tsx

--- src/fundraisers.tsx
+++ src/fundraisers.tsx
@@ -13,11 +13,15 @@ interface Fundraiser {
 // Each beneficiary address needs to be whitelisted on the lottery contract
 // with the `setBeneficiary` function, otherwise the transactions will fail.
 // If you don't want to show any fundraisers, set this to an empty array.
+
 export const FUNDRAISERS: Fundraiser[] = [
+   /*
   {
     title: "LottoPGF Support Fund",
     description: "Support development of the LottoPGF protocol!",
     targetAmount: parseUnits("0.42069", PRIZE_TOKEN_DECIMALS),
     address: "0x8220B74b87D77b11f6950dD2dFCe77D5D8971829",
+  
   },
+*/
 ];
