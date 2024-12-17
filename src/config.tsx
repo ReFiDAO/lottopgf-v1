a79a4406e52e60c8aaff345f1015d84edd846d14
ReFi DAO <167816643+ReFiDAO@users.noreply.github.com>
2024-12-17 18:51:35 +0000

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -13,17 +13,17 @@ export const METADATA = {
   bannerImage: "/images/ReFi DAO Holiday LottoPGF.png", 
   longDescription: (
     <p>
-      This holiday season, we’re launching The ReFi DAO Holiday Lotto from <b>Dec 18th - 24th</b>!
-      <br></br>
+      This holiday season, we’ve collaborated with LottoPGF to launch The ReFi DAO Holiday Lotto from <b>Dec 18th - 24th</b>!
+      <br></br><br></br>
       🌟 Kickstarted with $200 from Glo Dollar and ReFi DAO Global, the prize pool grows further with every ticket sold! 
-      If no lucky numbers are drawn on Christmas Eve, the winnings split evenly among ticket holders.
-      <br></br>
+      If no lucky numbers are drawn on Christmas Eve, the winnings are split evenly among ticket holders.
+      <br></br><br></br>
       🎟 Tickets are $10 USDGLO via Base Network - bridge or swap tokens at https://jumper.exchange.
       Each ticket sale also fuels ReFi DAO Global or ReFi Podcast.
-      <br></br>
+      <br></br><br></br>
       Get started below or watch this short video to find out more: https://tinyurl.com/WhatIsLottoPGF.
-      <br></br>
-      This is just the beginning—more exciting experiments coming in 2025! 🌱🎅 <b>Join the fun, fund the future.</b>
+      <br></br><br></br>
+      This is just the beginning—more exciting experiments coming in 2025! 🌱🎅
     </p>
   ),
 };
